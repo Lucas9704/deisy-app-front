30852c579535b14428afba781c81dda350320716
Lucas <luks9@DESKTOP-CLTLFVJ>
2022-08-04 16:54:39 -0300

    env vars

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://deisy-back.herokuapp.com/api/v1/",
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 5000,
 });
 
